b115c81fc857e472835c65a44b0d79ebc9c62ba5
Abbas Lamouri <abbas@Abbas-Omen.GENVAC.local>
2022-01-31 09:51:54 -0500

    M

--- components/products/AdminProductVariant.vue
+++ components/products/AdminProductVariant.vue
@@ -1,53 +1,53 @@
 <script setup>
-const props = defineProps({
-  prodVariant: {
-    type: Object,
-    required: true,
-  },
-  i: {
-    type: Number,
-    required: true,
-  },
-})
+	const props = defineProps({
+		prodVariant: {
+			type: Object,
+			required: true,
+		},
+		i: {
+			type: Number,
+			required: true,
+		},
+	})
 
-defineEmits(['mediaSelectorClicked', 'showVariantSlideout'])
+	defineEmits(['mediaSelectorClicked', 'showVariantSlideout'])
 
-// const props = defineProps({
-//   // variant: Object,
-//   index: Number,
-// })
+	// const props = defineProps({
+	//   // variant: Object,
+	//   index: Number,
+	// })
 
-const prodState = inject('prodState')
-const attState = inject('attState')
-const attTermsState = inject('attTermsState')
-const showActions = ref(false)
-const showVariantSlideout = ref(false)
+	const prodState = inject('prodState')
+	const attState = inject('attState')
+	const attTermsState = inject('attTermsState')
+	const showActions = ref(false)
+	const showVariantSlideout = ref(false)
 
-const getAttribute = (attributeId) => {
-  return prodState.selectedItem.attributes.filter((el) => el.item._id == attributeId)[0].item
-}
+	const getAttribute = (attributeId) => {
+		return prodState.selectedItem.attributes.filter((el) => el.item._id == attributeId)[0].item
+	}
 
-const getTerms = (attributeId) => {
-  const terms = prodState.selectedItem.attributes.filter((el) => el.item._id == attributeId)[0].terms
-  return terms
-}
+	const getTerms = (attributeId) => {
+		const terms = prodState.selectedItem.attributes.filter((el) => el.item._id == attributeId)[0].terms
+		return terms
+	}
 
-const removeProductVariant = () => {
-  if (!confirm('Are you sure?')) return
-  prodState.selectedItem.variants.splice(props.i, 1)
-}
+	const removeProductVariant = () => {
+		if (!confirm('Are you sure?')) return
+		prodState.selectedItem.variants.splice(props.i, 1)
+	}
 
-const updateVariant = (attribute, termId) => {
-  console.log('AT', attribute)
-  // console.log(value)
-  const term = attribute.terms.find((t) => t._id == termId)
+	const updateVariant = (attribute, termId) => {
+		console.log('AT', attribute)
+		// console.log(value)
+		const term = attribute.terms.find((t) => t._id == termId)
 
-  console.log('T', term)
+		console.log('T', term)
 
-  // if (!prodState.selectedItem.variants[props.i].attrTerms.length) {
-  prodState.selectedItem.variants[props.i].attrTerms.push(term)
-  // }
-}
+		// if (!prodState.selectedItem.variants[props.i].attrTerms.length) {
+		prodState.selectedItem.variants[props.i].attrTerms.push(term)
+		// }
+	}
 </script>
 
 <!-- &&
@@ -55,51 +55,47 @@ const updateVariant = (attribute, termId) => {
       prodState.selectedItem.variants[i].attrTerms.length -->
 
 <template>
-  <div class="admin-product-variant">
-        <!-- <pre style="font-size: 1rem">{{ prodState.selectedItem }}</pre> -->
+	<div class="admin-product-variant">
+		<div class="image td" @click="$emit('showVariantSlideout')">
+			<img v-if="prodVariant.gallery[0]" :src="prodVariant.gallery[0].path" alt="Variant Image" />
+			<img v-else src="/placeholder.png" alt="Variant Image" />
+		</div>
+		<div class="option td" @click="$emit('showVariantSlideout')">
+			<div v-for="term in prodVariant.attrTerms" :key="term" class="attribute-term">
+				<div class="attribute">
+					{{ attState.items.find((a) => a._id == term.parent).name }}
+				</div>
+				<div class="term">
+					{{ attTermsState.items.find((t) => t._id == term._id).name }}
+				</div>
+			</div>
+		</div>
+		<div class="stock-qty td">
+			<div v-if="!prodState.selectedItem.manageInventory">&infin;</div>
+			<div v-else>{{ prodVariant.stockQty }}</div>
+		</div>
+		<div class="price td">{{ prodVariant.price }}</div>
+		<div class="sku td">{{ prodVariant.sku }}</div>
+		<div class="actions td">
+			<button class="btn" @click.prevent="showActions = !showActions"><IconsMoreHoriz /></button>
+			<div class="menu shadow-md" v-show="showActions">
+				<a href="#" class="link"><div class="advanced">Advanced</div></a>
+				<a href="#" class="link" @click.prevent="removeProductVariant">
+					<div class="cancel">Delete</div>
+				</a>
+				<a href="#" class="link" @click.prevent="attState.items.splice(i, 1)">
+					<div class="cancel">Cancel</div>
+				</a>
+			</div>
+		</div>
 
-
-    <!-- <div class="td"> -->
-    <div class="image td" @click="$emit('showVariantSlideout')">
-      <img v-if="prodVariant.gallery[0]" :src="prodVariant.gallery[0].path" alt="Variant Image" />
-      <img v-else src="/placeholder.png" alt="Variant Image" />
-    </div>
-    <div class="option td" @click="$emit('showVariantSlideout')">
-      <div v-for="term in prodVariant.attrTerms" :key="term" class="attribute-term">
-        <div class="attribute">
-          {{ attState.items.find((a) => a._id == term.parent).name}}
-        </div>
-        <div class="term">
-          {{ attTermsState.items.find((t) => t._id == term._id).name }}
-        </div>
-      </div>
-    </div>
-    <div class="stock-qty td">
-      <div v-if="!prodState.selectedItem.manageInventory">&infin;</div>
-      <div v-else>{{ prodVariant.stockQty }}</div>
-    </div>
-    <div class="price td">{{ prodVariant.price }}</div>
-    <div class="sku td">{{ prodVariant.sku }}</div>
-    <div class="actions td">
-      <button class="btn" @click.prevent="showActions = !showActions"><IconsMoreHoriz /></button>
-      <div class="menu shadow-md" v-show="showActions">
-        <a href="#" class="link"><div class="advanced">Advanced</div></a>
-        <a href="#" class="link" @click.prevent="removeProductVariant">
-          <div class="cancel">Delete</div>
-        </a>
-        <a href="#" class="link" @click.prevent="attState.items.splice(i, 1)">
-          <div class="cancel">Cancel</div>
-        </a>
-      </div>
-    </div>
-
-    <!-- <div class="actions td">
+		<!-- <div class="actions td">
             <IconsDeleteFill @click="removeProductAttribute" />
           </div> -->
 
-    <!-- </div> -->
+		<!-- </div> -->
 
-    <!-- <form @keypress.enter.prevent>
+		<!-- <form @keypress.enter.prevent>
           <ProductsAdminAttribute
             :attribute="attribute"
             :i="i"
@@ -114,8 +110,8 @@ const updateVariant = (attribute, termId) => {
           />
           <button class="btn btn-primary" @click="saveAttributes">Save Changes</button>
         </form> -->
-  </div>
-  <!-- <div class="variant space-y-4 border p-6" v-if="prodState.selectedItem.variants[i]">
+	</div>
+	<!-- <div class="variant space-y-4 border p-6" v-if="prodState.selectedItem.variants[i]">
     <div class="header bg-blue-100 flex gap-8 py-4 justify-between">
       <div class="flex gap-8 py-4 justify-between">
         <div
@@ -252,47 +248,52 @@ const updateVariant = (attribute, termId) => {
 </template>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/variables';
+	@import '@/assets/scss/variables';
 
-.admin-product-variant {
-  cursor: pointer;
+	.admin-product-variant {
+		cursor: pointer;
 
-  .td {
-    display: flex;
-    align-items: center;
-  }
+		.td {
+			display: flex;
+			align-items: center;
+		}
 
-  .image {
-    width: 5rem;
-    height: 5rem;
+		.image {
+			width: 5rem;
+			height: 5rem;
 
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-  }
+			img {
+				width: 100%;
+				height: 100%;
+				object-fit: cover;
+			}
+		}
 
-  .option {
-    display: flex;
-    .attribute-term {
-      display: flex;
-      align-items: center;
-      // cursor: pointer;
+		.option {
+			display: flex;
+			.attribute-term {
+				display: flex;
+				align-items: center;
+				// cursor: pointer;
 
-      .attribute {
-        background-color: $slate-400;
-        color: white;
-        padding: 0.5rem 1.5rem;
-        border-radius: 2rem 0 0 2rem;
-      }
+				.attribute {
+					background-color: $slate-400;
+					color: white;
+					padding: 0.25rem 0.5rem;
+					border-radius: 2rem 0 0 2rem;
+				}
 
-      .term {
-        background-color: $slate-200;
-        padding: 0.5rem 1.5rem;
-        border-radius: 0 2rem 2rem 0;
-      }
-    }
-  }
-}
+				.term {
+					background-color: $slate-200;
+					padding: 0.25rem 0.5rem;
+					border-radius: 0 2rem 2rem 0;
+				}
+			}
+		}
+
+		.actions {
+			display: flex;
+			justify-content: flex-end;
+		}
+	}
 </style>
--- pages/admin/products/[slug].vue
+++ pages/admin/products/[slug].vue
@@ -1,282 +1,282 @@
 <script setup>
-import slugify from 'slugify'
-const route = useRoute()
+	import slugify from 'slugify'
+	const route = useRoute()
 
-// Import products, categories, attributes, atterbute terms and variants state and actions
-const { state: prodState, actions: prodActions } = useFactory('products')
-const { state: catState, actions: catActions } = useFactory('categories')
-const { state: attState, actions: attActions } = useFactory('attributes')
-const { state: attTermsState, actions: attTermsActions } = useFactory('attributeterms')
-const { state: variantState, actions: variantActions } = useFactory('variants')
+	// Import products, categories, attributes, atterbute terms and variants state and actions
+	const { state: prodState, actions: prodActions } = useFactory('products')
+	const { state: catState, actions: catActions } = useFactory('categories')
+	const { state: attState, actions: attActions } = useFactory('attributes')
+	const { state: attTermsState, actions: attTermsActions } = useFactory('attributeterms')
+	const { state: variantState, actions: variantActions } = useFactory('variants')
 
-const showAttVarSlideout = ref(false)
+	const showAttVarSlideout = ref(false)
 
-// Set product filters
-prodState.query.slug = route.params.slug
-prodState.query.populate =
-  'featuredImage gallery categories attributes attributes.attribute attributes.terms attributes.defaultTerm'
+	// Set product filters
+	prodState.query.slug = route.params.slug
+	prodState.query.populate =
+		'featuredImage gallery categories attributes attributes.attribute attributes.terms attributes.defaultTerm'
 
-// fetch product, categories, attributes and attribute terms
-await Promise.all([prodActions.fetchAll(), catActions.fetchAll(), attActions.fetchAll(), attTermsActions.fetchAll()])
+	// fetch product, categories, attributes and attribute terms
+	await Promise.all([prodActions.fetchAll(), catActions.fetchAll(), attActions.fetchAll(), attTermsActions.fetchAll()])
 
-if (prodState.items.length) {
-  prodState.selectedItem = prodState.items[0]
-  variantState.query.populate = 'attrTerms featuredImage'
-  variantState.query.product = prodState.selectedItem._id
-  if (variantState.query.product) await variantActions.fetchAll()
-  prodState.selectedItem.variants = variantState.items
-} else {
-  prodState.selectedItem = {
-    name: 'Hello',
-    slug: computed(() => slugify(prodState.selectedItem.name, { lower: true })),
-    permalink: computed(() => slugify(prodState.selectedItem.name, { lower: true })),
-    price: 125,
-    active: true,
-    attributes: [],
-    categories: [],
-    gallery: [],
-    taxStatus: 'none',
-    taxClass: 'standard',
-    allowBcakOrder: 'notify',
-    variants: [],
-  }
-}
+	if (prodState.items.length) {
+		prodState.selectedItem = prodState.items[0]
+		variantState.query.populate = 'attrTerms gallery'
+		variantState.query.product = prodState.selectedItem._id
+		if (variantState.query.product) await variantActions.fetchAll()
+		prodState.selectedItem.variants = variantState.items
+	} else {
+		prodState.selectedItem = {
+			name: 'Hello',
+			slug: computed(() => slugify(prodState.selectedItem.name, { lower: true })),
+			permalink: computed(() => slugify(prodState.selectedItem.name, { lower: true })),
+			price: 125,
+			active: true,
+			attributes: [],
+			categories: [],
+			gallery: [],
+			taxStatus: 'none',
+			taxClass: 'standard',
+			allowBcakOrder: 'notify',
+			variants: [],
+		}
+	}
 
-const showMediaSelector = ref(false) // media selector toggler
-const mediaReference = ref({}) // sets which media to update once a selection is made
+	const showMediaSelector = ref(false) // media selector toggler
+	const mediaReference = ref({}) // sets which media to update once a selection is made
 
-const handleMediaSelectorClick = (payload) => {
-  showMediaSelector.value = true
-  console.log(payload)
-  mediaReference.value = payload
-}
+	const handleMediaSelectorClick = (payload) => {
+		showMediaSelector.value = true
+		console.log(payload)
+		mediaReference.value = payload
+	}
 
-const processSelectedMedia = async (media) => {
-  showMediaSelector.value = false
-  media = media.filter((el) => el.mimetype.includes('image'))
+	const processSelectedMedia = async (media) => {
+		showMediaSelector.value = false
+		media = media.filter((el) => el.mimetype.includes('image'))
 
-  if (mediaReference.value.image === 'variant') {
-    for (const prop in media) {
-      let index = prodState.selectedItem.variants[mediaReference.value.index].gallery.findIndex(
-        (el) => el._id === media[prop]._id
-      )
-      if (index === -1) {
-        prodState.selectedItem.variants[mediaReference.value.index].gallery.push(media[prop])
-      }
+		if (mediaReference.value.image === 'variant') {
+			for (const prop in media) {
+				let index = prodState.selectedItem.variants[mediaReference.value.index].gallery.findIndex(
+					(el) => el._id === media[prop]._id
+				)
+				if (index === -1) {
+					prodState.selectedItem.variants[mediaReference.value.index].gallery.push(media[prop])
+				}
 
-      index = prodState.selectedItem.gallery.findIndex((el) => el._id === media[prop]._id)
-      if (index === -1) {
-        prodState.selectedItem.gallery.push(media[prop])
-      }
-    }
-  }
+				index = prodState.selectedItem.gallery.findIndex((el) => el._id === media[prop]._id)
+				if (index === -1) {
+					prodState.selectedItem.gallery.push(media[prop])
+				}
+			}
+		}
 
-  if (mediaReference.value.image === 'gallery') {
-    for (const prop in media) {
-      const index = prodState.selectedItem.gallery.findIndex((el) => el._id === media[prop]._id)
-      if (index === -1) {
-        prodState.selectedItem.gallery.push(media[prop])
-      }
-    }
-  }
-}
+		if (mediaReference.value.image === 'gallery') {
+			for (const prop in media) {
+				const index = prodState.selectedItem.gallery.findIndex((el) => el._id === media[prop]._id)
+				if (index === -1) {
+					prodState.selectedItem.gallery.push(media[prop])
+				}
+			}
+		}
+	}
 
-provide('prodState', prodState)
-provide('prodActions', prodActions)
-provide('catState', catState)
-provide('attState', attState)
-provide('attTermsState', attTermsState)
-provide('variantState', variantState)
-provide('variantActions', variantActions)
-provide('handleMediaSelectorClick', handleMediaSelectorClick)
-provide('processSelectedMedia', processSelectedMedia)
-provide('showAttVarSlideout', showAttVarSlideout)
+	provide('prodState', prodState)
+	provide('prodActions', prodActions)
+	provide('catState', catState)
+	provide('attState', attState)
+	provide('attTermsState', attTermsState)
+	provide('variantState', variantState)
+	provide('variantActions', variantActions)
+	provide('handleMediaSelectorClick', handleMediaSelectorClick)
+	provide('processSelectedMedia', processSelectedMedia)
+	provide('showAttVarSlideout', showAttVarSlideout)
 </script>
 
 <script>
-export default {
-  layout: 'admin',
-}
+	export default {
+		layout: 'admin',
+	}
 </script>
 
 <template>
-  <div class="product-details">
-    <pre style="font-size: 1rem">{{ prodState.selectedItem }}</pre>
-    <Html>
-      <Head><Title>Product</Title></Head>
-    </Html>
+	<div class="product-details">
+		<!-- <pre style="font-size: 1rem">{{ prodState.selectedItem }}</pre> -->
+		<Html>
+			<Head><Title>Product</Title></Head>
+		</Html>
 
-    <NuxtLink class="link" :to="{ name: 'admin-products' }"> <IconsArrowWest /><span>Products</span> </NuxtLink>
+		<NuxtLink class="link" :to="{ name: 'admin-products' }"> <IconsArrowWest /><span>Products</span> </NuxtLink>
 
-    <h3 class="header">Edit Product</h3>
+		<h3 class="header">Edit Product</h3>
 
-    <div class="columns">
-      <div class="left shadow-md">
-        <ProductsAdminProductNav />
-      </div>
+		<div class="columns">
+			<div class="left shadow-md">
+				<ProductsAdminProductNav />
+			</div>
 
-      <div class="center">
-        <ProductsAdminDetails />
-        <ProductsAdminPrice />
-        <ProductsAdminProductImageGallery />
+			<div class="center">
+				<ProductsAdminDetails />
+				<ProductsAdminPrice />
+				<ProductsAdminProductImageGallery />
 
-        <section class="variants" id="variants">
-          <header class="header flex-bc">
-            <div class="admin-section-header">Variants</div>
-            <button class="btn btn-primary flex-cc" @click="showAttVarSlideout = true">
-              <IconsPlus />
-              <span>Add</span>
-            </button>
-          </header>
+				<section class="variants" id="variants">
+					<header class="header flex-bc">
+						<div class="admin-section-header">Variants</div>
+						<button class="btn btn-primary flex-cc" @click="showAttVarSlideout = true">
+							<IconsPlus />
+							<span>Add</span>
+						</button>
+					</header>
 
-          <div class="content">
-            <div>Different types of this product (e.g. size, color)</div>
-          </div>
-        </section>
+					<div class="content">
+						<div>Different types of this product (e.g. size, color)</div>
+					</div>
+				</section>
 
-        <ProductsAdminVariants />
-        <ProductsAdminShippingOptions />
-        <ProductsAdminDigitalDelivery />
-        <ProductsAdminExtraFields />
-        <ProductsAdminSeo />
-        <ProductsAdminMisc />
-      </div>
+				<ProductsAdminVariants />
+				<ProductsAdminShippingOptions />
+				<ProductsAdminDigitalDelivery />
+				<ProductsAdminExtraFields />
+				<ProductsAdminSeo />
+				<ProductsAdminMisc />
+			</div>
 
-      <div class="right">
-        <ProductsAdminRightNav />
-      </div>
-    </div>
-    <div class="media-selector" v-if="showMediaSelector">
-      <LazyMediaUploader
-        @mediaSelected="processSelectedMedia"
-        @mediaSelectCancel="showMediaSelector = false"
-        v-if="showMediaSelector"
-      />
-    </div>
-  </div>
+			<div class="right">
+				<ProductsAdminRightNav />
+			</div>
+		</div>
+		<div class="media-selector" v-if="showMediaSelector">
+			<LazyMediaUploader
+				@mediaSelected="processSelectedMedia"
+				@mediaSelectCancel="showMediaSelector = false"
+				v-if="showMediaSelector"
+			/>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/variables';
+	@import '@/assets/scss/variables';
 
-.product-details {
-  max-width: 1280px;
-  min-height: 100vh;
-  padding: 2rem;
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  .link {
-    display: flex;
-    align-items: center;
-    gap: 0.3rem;
+	.product-details {
+		max-width: 1280px;
+		min-height: 100vh;
+		padding: 2rem;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+		.link {
+			display: flex;
+			align-items: center;
+			gap: 0.3rem;
 
-    svg {
-      width: 1.8rem;
-      height: 1.8rem;
-    }
-  }
+			svg {
+				width: 1.8rem;
+				height: 1.8rem;
+			}
+		}
 
-  .columns {
-    display: grid;
-    grid-template-columns: 18rem 1fr 25rem;
-    gap: 2rem;
-    align-items: flex-start;
+		.columns {
+			display: grid;
+			grid-template-columns: 18rem 1fr 25rem;
+			gap: 2rem;
+			align-items: flex-start;
 
-    .left {
-      position: sticky;
-      top: 10rem;
-      background-color: white;
-      border: 1px solid $slate-100;
-      border-radius: 3px;
-      padding: 2rem 0.5rem;
-    }
+			.left {
+				position: sticky;
+				top: 10rem;
+				background-color: white;
+				border: 1px solid $slate-100;
+				border-radius: 3px;
+				padding: 2rem 0.5rem;
+			}
 
-    .center {
-      display: flex;
-      flex-direction: column;
-      gap: 3rem;
+			.center {
+				display: flex;
+				flex-direction: column;
+				gap: 3rem;
 
-      .variants {
-        background-color: white;
-        border-radius: 5px;
-        padding: 2rem 2rem;
+				.variants {
+					background-color: white;
+					border-radius: 5px;
+					padding: 2rem 2rem;
 
-        .header {
-          .btn {
-            gap: 0.25rem;
+					.header {
+						.btn {
+							gap: 0.25rem;
 
-            svg {
-              fill: $slate-50;
-            }
-          }
-        }
-      }
-      // z-index:-1;
+							svg {
+								fill: $slate-50;
+							}
+						}
+					}
+				}
+				// z-index:-1;
 
-      // .details {
-      //   background-color: white;
-      //   border-radius: 5px;
-      //   padding: 2rem 2rem;
+				// .details {
+				//   background-color: white;
+				//   border-radius: 5px;
+				//   padding: 2rem 2rem;
 
-      //   .info {
-      //     display: flex;
-      //     flex-direction: column;
-      //     gap: 1rem;
+				//   .info {
+				//     display: flex;
+				//     flex-direction: column;
+				//     gap: 1rem;
 
-      //     .sku-inventory {
-      //       display: flex;
-      //       align-items: center;
-      //       justify-content: space-between;
-      //       gap: 2rem;
+				//     .sku-inventory {
+				//       display: flex;
+				//       align-items: center;
+				//       justify-content: space-between;
+				//       gap: 2rem;
 
-      //       .sku {
-      //         flex: 1;
-      //       }
+				//       .sku {
+				//         flex: 1;
+				//       }
 
-      //       .inventory {
-      //         display: flex;
-      //         flex-direction: column;
-      //         gap: 0.5rem;
-      //         font-size: 1.3rem;
+				//       .inventory {
+				//         display: flex;
+				//         flex-direction: column;
+				//         gap: 0.5rem;
+				//         font-size: 1.3rem;
 
-      //         .available {
-      //           display: flex;
-      //           align-items: center;
-      //           gap: 1rem;
+				//         .available {
+				//           display: flex;
+				//           align-items: center;
+				//           gap: 1rem;
 
-      //           .title {
-      //             font-weight: 600;
-      //           }
-      //         }
-      //       }
-      //     }
-      //   }
-      // }
+				//           .title {
+				//             font-weight: 600;
+				//           }
+				//         }
+				//       }
+				//     }
+				//   }
+				// }
 
-      // .price {
-      //   background-color: white;
-      //   border-radius: 5px;
-      //   padding: 2rem 2rem;
-      // }
-    }
+				// .price {
+				//   background-color: white;
+				//   border-radius: 5px;
+				//   padding: 2rem 2rem;
+				// }
+			}
 
-    .right {
-      position: sticky;
-      top: 10rem;
-      display: flex;
-      flex-direction: column;
-      gap: 2rem;
-    }
-  }
+			.right {
+				position: sticky;
+				top: 10rem;
+				display: flex;
+				flex-direction: column;
+				gap: 2rem;
+			}
+		}
 
-  .link {
-    font-weight: 500;
-    color: $slate-400;
+		.link {
+			font-weight: 500;
+			color: $slate-400;
 
-    &:hover {
-      color: $slate-800;
-    }
-  }
-}
+			&:hover {
+				color: $slate-800;
+			}
+		}
+	}
 </style>
